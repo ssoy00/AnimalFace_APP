f35be7dd107c720ec43c1739b4bae0165dee63ed
KMS9637 <kmsa0202@gmail.com>
2024-08-19 01:09:38 +0900

    로그인, 회원가입, 메인화면에서 (로그인/회원가입)화면으로 이동하는 버튼 생성

--- app/src/main/java/com/project/animalface_app/MainActivity.kt
+++ app/src/main/java/com/project/animalface_app/MainActivity.kt
@@ -1,16 +1,41 @@
 package com.project.animalface_app
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.project.animalface_app.kmsapp.LoginActivity
+import com.project.animalface_app.kmsapp.RegisterActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var buttonGoToRegister: Button
+    private lateinit var buttonGoToLogin: Button
+
+//    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+//        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        buttonGoToRegister = findViewById(R.id.buttonGoToRegister)
+        buttonGoToLogin = findViewById(R.id.buttonGoToLogin)
+
+        // 회원가입 페이지로 이동
+        buttonGoToRegister.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
+        // 로그인 페이지로 이동
+        buttonGoToLogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
---
+++ app/src/main/java/com/project/animalface_app/kmsapp/LoginActivity.kt
@@ -0,0 +1,28 @@
+package com.project.animalface_app.kmsapp
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.project.animalface_app.R
+
+class LoginActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        // main이라는 ID가 없으면 이 부분을 제거하거나 적절한 뷰에 적용하세요.
+        val mainView = findViewById<View>(R.id.main)
+        mainView?.let {
+            ViewCompat.setOnApplyWindowInsetsListener(it) { v, insets ->
+                val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+                insets
+            }
+        }
+    }
+}
