f739c2c5c32234f44a59fd1b8a30a1de71872f2b
kimsoyoung <ggomag3243@gmail.com>
2024-08-23 20:29:41 +0900

    앱 rest 서버 연동 확인 중...

---
+++ app/src/main/java/com/project/animalface_app/ksyapp/RetrofitClient.kt
@@ -0,0 +1,26 @@
+package com.project.animalface_app.ksyapp
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+    private const val BASE_URL = "http://localhost:8080/api/notices/{noticeNo}"
+
+    private val loggingInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    private val client = OkHttpClient.Builder()
+        .addInterceptor(loggingInterceptor)
+        .build()
+
+    val retrofitInstance: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+}
